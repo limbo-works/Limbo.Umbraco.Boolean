12cc83207408b58a0d3fdeb0a4b35bf9a77c17c5
Anders Bjerner <abjerner@skybrud.dk>
2021-11-23 16:45:38 +0100

    Initial commit

---
+++ src/Limbo.Umbraco.Boolean/Editors/Boolean/BooleanEditor.cs
@@ -0,0 +1,35 @@
+﻿using Umbraco.Core.Logging;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Web.PropertyEditors;
+
+namespace Limbo.Umbraco.Boolean.Editors.Boolean {
+    
+    /// <summary>
+    /// Represents a checkbox property and parameter editor.
+    /// </summary>
+    [DataEditor(
+        EditorAlias,
+        EditorType.PropertyValue | EditorType.MacroParameter,
+        "Limbo Boolean",
+        "boolean",
+        ValueType = ValueTypes.Integer,
+        Group = "Limbo",
+        Icon = "icon-checkbox color-limbo")]
+    public class BooleanEditor : DataEditor {
+
+        /// <summary>
+        /// Gets the alias of the editor.
+        /// </summary>
+        public const string EditorAlias = "Limbo.Boolean";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BooleanEditor"/> class.
+        /// </summary>
+        public BooleanEditor(ILogger logger) : base(logger) { }
+
+        /// <inheritdoc />
+        protected override IConfigurationEditor CreateConfigurationEditor() => new TrueFalseConfigurationEditor();
+
+    }
+
+}
---
+++ src/Limbo.Umbraco.Boolean/Editors/Boolean/BooleanValueConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Web.PropertyEditors;
+
+namespace Limbo.Umbraco.Boolean.Editors.Boolean {
+    
+    public class BooleanValueConverter : PropertyValueConverterBase {
+        
+        public override bool IsConverter(IPublishedPropertyType propertyType) {
+            return propertyType.EditorAlias == BooleanEditor.EditorAlias;
+        }
+
+        public override Type GetPropertyValueType(IPublishedPropertyType propertyType) {
+            return typeof(bool);
+        }
+
+        public override PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType) {
+            return PropertyCacheLevel.Element;
+        }
+
+        public override object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview) {
+
+            TrueFalseConfiguration config = propertyType.DataType.ConfigurationAs<TrueFalseConfiguration>();
+
+            return source switch  {
+                string s => bool.TryParse(s, out bool result) ? result : config.Default,
+                int i => i == 1,
+                long l => l == 1,
+                bool b => b,
+                _ => config.Default
+            };
+
+        }
+
+        public override object ConvertIntermediateToXPath(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview) {
+            return (bool) inter ? "1" : "0";
+        }
+
+    }
+
+}
