2cd36fe0579a5392ec4a841534b5e83e27ab9ce2
Anders Bjerner <abjerner@skybrud.dk>
2022-08-22 18:26:27 +0200

    Removed unnecessary whitespace

--- src/Limbo.Umbraco.Boolean/Editors/Boolean/BooleanValueConverter.cs
+++ src/Limbo.Umbraco.Boolean/Editors/Boolean/BooleanValueConverter.cs
@@ -3,27 +3,27 @@ using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.PropertyEditors;
 
 namespace Limbo.Umbraco.Boolean.Editors.Boolean {
-    
+
     /// <summary>
     /// Value converter for the <see cref="BooleanEditor"/> property editor.
     /// </summary>
     public class BooleanValueConverter : PropertyValueConverterBase {
-        
+
         /// <inheritdoc />
         public override bool IsConverter(IPublishedPropertyType propertyType) {
             return propertyType.EditorAlias == BooleanEditor.EditorAlias;
         }
-        
+
         /// <inheritdoc />
         public override Type GetPropertyValueType(IPublishedPropertyType propertyType) {
             return typeof(bool);
         }
-        
+
         /// <inheritdoc />
         public override PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType) {
             return PropertyCacheLevel.Element;
         }
-        
+
         /// <inheritdoc />
         public override object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview) {
 
@@ -38,7 +38,7 @@ namespace Limbo.Umbraco.Boolean.Editors.Boolean {
             };
 
         }
-        
+
         /// <inheritdoc />
         public override object ConvertIntermediateToXPath(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview) {
             return (bool) inter ? "1" : "0";
